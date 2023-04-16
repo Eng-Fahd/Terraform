8bf418800c45109d4049377a1d8bafc3036a0387
Fahd Khaled <118731346+Eng-Fahd@users.noreply.github.com>
2023-04-17 01:43:41 +0200

    Adding RDS Secrets Manager

--- db.tf
+++ db.tf
@@ -11,11 +11,9 @@ resource "aws_db_instance" "example_rds" {
   engine_version         = "5.7"
   instance_class         = "db.t3.micro"
   db_name                = "rdsTerraform"
-  username               = "admin"
-  password               = "password"
+  username               = jsondecode(data.aws_secretsmanager_secret_version.current_secrets.secret_string)["username"]
+  password               = jsondecode(data.aws_secretsmanager_secret_version.current_secrets.secret_string)["password"]
   skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.rds_security_group.id]
   db_subnet_group_name   = aws_db_subnet_group.example_rds_subnet_group.id
 }
-
-# db Connect mysql -h DB-ARN -P 3306 -u USER -p
